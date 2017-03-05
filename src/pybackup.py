c41fe6a6bec1e620289eadda1ea5b8aa3563aa31
Andres Bott <contact@andresbott.com>
2017-03-05 16:16:07 +0100

    working on restore, refactor parameters to use -restore backupfile -conf confile

--- src/pybackup.py
+++ src/pybackup.py
@@ -7,13 +7,14 @@ def main(argv):
     inputfile = ''
 
     parser = argparse.ArgumentParser(description='Crate mysql and Files backup based on profile File definition')
-    parser.add_argument('-run', metavar='configFile',   help='run a backup job with the specified config file', type=argparse.FileType('r'))
-    parser.add_argument('-restore', metavar='restorePath',   help='Restore a tgz compressed with pybackuo to the desired path', type=argparse.FileType('r'))
+    parser.add_argument('-backup', metavar='configFile',   help='run a backup job with the specified config file', type=argparse.FileType('r'))
+    parser.add_argument('-restore', metavar='configFile',   help='run a restore job with the specified config file', type=argparse.FileType('r'))
+    parser.add_argument('-restorefile', nargs=2 , metavar=('configFile','backupFile'),     help='run a restore job with the specified config file and the backupfile', type=argparse.FileType('r'))
     args = parser.parse_args()
 
-    if args.run is not None:
+    if args.backup is not None:
         isSuperCow()
-        inputfile = args.run.name
+        inputfile = args.backup.name
         backupparms =  {
             "profilename":"",
             "rootdir":"",
@@ -26,10 +27,34 @@ def main(argv):
             "tmpdir":"../pybackup_tmp",
             "destination":"",
         }
-        config = BackupConfig(inputfile,'pybackup',backupparms)
+        config = PyBackupConfig(inputfile,'pybackup',backupparms)
         # config.printer()
-        backup = BackupWorker(config)
-        backup.run()
+        backup = pyBackupWorker(config)
+        backup.runBackup()
+    elif args.restore is not None:
+        isSuperCow()
+        inputfile = args.restore.name
+        restoreParams =  {
+            "rootdir":"",
+            # "excludes":"",
+            "mysqldb":"!",
+            "mysqldbfileName":"mysqldump.sql",
+            "tmpdir":"../pybackup_tmp",
+        }
+        config = PyBackupConfig(inputfile,'pybackup',restoreParams)
+        restore = pyBackupWorker(config)
+        restore.runRestore()
+    elif args.restorefile is not None:
+
+        isSuperCow()
+        configFile = args.restorefile[0].name
+        restoreFile = args.restorefile[1].name
+
+        print (configFile)
+        print (restoreFile)
+        print (args)
+
+
 
     else:
         parser.print_help()
@@ -40,66 +65,36 @@ def isSuperCow():
     if not os.geteuid()==0:
         sys.exit("\nOnly root can run this script\n")
 
-
-class BackupConfig:
-    """
-    Class To handle the needed configuration
-    will read a inifile in the consturctor and fill in the dictionoary with the needed values
-    if a value is defined it will assign it in the exception
-    """
+class PyBackupConfig:
 
     configFile =""
     section = ""
     values= {}
 
 
-    def __init__(self,file,section,values):
-        """
-        parse a inifile and add the defined values to the config instance
-        :param file: file to be loaded
-        :return: none
-        """
+    def __init__(self,file,section,defaults):
         self.configFile = os.path.abspath(file)
         self.section = section
-        self.values =  values
 
         try:
-            open(file)
-            config = configparser.ConfigParser()
-            config.read(file)
+            open(self.configFile)
+            config = configparser.SafeConfigParser(defaults)
+            config.read(self.configFile)
 
-            for param in self.values:
-                try:
-                    value = config.get(self.section, param)
-                    value = value.strip("'")
-                    value = value.strip('"')
-                    self.values[param] =value
-                except configparser.NoOptionError:
-                    if self.values[param] == "" :
-                        e = str(sys.exc_info()[1])
-                        print("Error: "+e);
-                        sys.exit(1)
+
+            for param in defaults:
+
+                value = config.get(self.section, param)
+                value = value.strip("'")
+                value = value.strip('"')
+                self.values[param] =value
+
         except IOError:
             print("could not read", file)
             sys.exit(1)
 
-        # except: # catch *all* exceptions
-        #     e = str(sys.exc_info()[0])
-        #     print("Unexpected error: "+e);
-        #     sys.exit(1)
 
-    def printer(self):
-        """
-        Print out the values
-        :return: none
-        """
-        print ("====================")
-        for param, value in self.values.items() :
-            print (param+" : "+value)
-        print ("====================")
-
-
-class BackupWorker:
+class pyBackupWorker():
 
 
     mysqlconfigFiles = ["/root/.my.cnf","/etc/mysql/debian.cnf"]
@@ -109,7 +104,7 @@ class BackupWorker:
         # config.printer()
         self.config = config
 
-    def run(self):
+    def runBackup(self):
         self.createTmpDir()
         self.mysqlBackup()
         self.copyFiles()
@@ -117,6 +112,10 @@ class BackupWorker:
         self.compressTmp()
         self.clean()
         self.cleanOld()
+
+    def runRestore(self):
+        self.createTmpDir()
+        # self.mysqlRestore()
 
     def createTmpDir(self):
 
@@ -131,27 +130,33 @@ class BackupWorker:
             os.makedirs(self.tmpPath)
             os.chmod(self.tmpPath, 0o700)
 
-    def mysqlBackup(self):
+    def getMysqlConfig(self):
+        ret = {}
 
         if self.config.values["mysqldb"] != "!":
-            user = ""
-            passwd = ""
-            dbName = self.config.values["mysqldb"]
-            dumpPath = self.tmpPath+"/"+self.config.values["mysqldbfileName"]
+            ret["user"] = ""
+            ret["passwd"] = ""
+            ret["dbName"] = self.config.values["mysqldb"]
+            ret["dumpPath"] = self.tmpPath+"/"+self.config.values["mysqldbfileName"]
             mysqlconfigFiles = self.mysqlconfigFiles
             mysqlconfigFiles.append(self.config.configFile)
 
             for file in mysqlconfigFiles:
                 if os.path.isfile(file):
                     values = {"user":"","password":""}
-                    config = BackupConfig(file,"client",values)
-                    user = config.values["user"]
-                    passwd = config.values["password"]
+                    config = PyBackupConfig(file,"client",values)
+                    ret["user"] = config.values["user"]
+                    ret["passwd"] = config.values["password"]
                     break
+        return ret
 
-            args = ['mysqldump', '-u', user, "-p"+passwd, '--add-drop-database', '--databases', dbName ]
 
-            with open(dumpPath, 'wb', 0) as file:
+    def mysqlBackup(self):
+
+        if self.config.values["mysqldb"] != "!":
+            confdata = self.getMysqlConfig()
+            args = ['mysqldump', '-u', confdata["user"], "-p"+confdata["passwd"], '--add-drop-database', '--databases', confdata["dbName"] ]
+            with open(confdata["dumpPath"], 'wb', 0) as file:
                 p1 = Popen(args, stdout=file)
                 # p1 = Popen(args, stdout=PIPE)
                 # p2 = Popen('gzip', stdin=p1.stdout, stdout=f)
@@ -160,9 +165,22 @@ class BackupWorker:
             p1.wait()
 
             if self.config.values["chown"] != "!":
-                call(["chown",self.config.values["chown"]+":"+self.config.values["chown"],dumpPath])
+                call(["chown",self.config.values["chown"]+":"+self.config.values["chown"],confdata["dumpPath"]])
             if self.config.values["chmod"] != "!":
-                call(["chmod",self.config.values["chmod"],dumpPath])
+                call(["chmod",self.config.values["chmod"],confdata["dumpPath"]])
+
+    def mysqlRestore(self):
+        if self.config.values["mysqldb"] != "!":
+            confdata = self.getMysqlConfig()
+            # mysql -u <user> -p < db_backup.dump
+            args = ['mysql',  '-u', confdata["user"], "-p"+confdata["passwd"], "<", confdata["dumpPath"] ]
+            print (args)
+
+            # returncode = call(args)
+            #
+            # if returncode != 0:
+            #     print ("something Went wrong while importing Mysql dump file")
+            #     sys.exit(1)
 
 
     def copyFiles(self):
@@ -176,7 +194,7 @@ class BackupWorker:
         returncode = call(args)
 
         if returncode != 0:
-            print ("something Went wron while copying files with cp from: "+origin + " to: "+destination+", exiting")
+            print ("something Went wrong while copying files with cp from: "+origin + " to: "+destination+", exiting")
             sys.exit(1)
 
 
@@ -193,7 +211,7 @@ class BackupWorker:
             sys.exit(1)
 
     def compressTmp(self):
-        now = datetime.datetime.now().strftime('%Y-%m-%d=%H.%M.%S')
+        now = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
         origin = self.tmpPath
         destination = self.config.values["destination"]
         # 2017_01_01_-_04_22_01.aireyvuelo_com_backup.tgz
@@ -219,6 +237,14 @@ class BackupWorker:
         if self.config.values["chmod"] != "!":
             call(["chmod",self.config.values["chmod"],destinationFile])
 
+    def uncompressTmp(self):
+
+        if self.tmpPath is not "":
+            args = ["tar", "-zxf",destinationFile, "-C", origin, "./"]
+            returncode = call(args)
+            if returncode != 0:
+                print ("something Went wron while Compressing TAR from: "+origin + " to: "+destinationFile+", exiting")
+                sys.exit(1)
 
 
     def clean(self):
